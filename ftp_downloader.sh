361b52d8c255c0ff015d9aee6159d040c2c5be2f
Mauricio Figueroa <mauricio.figueroa@despegar.com>
2017-07-17 09:45:13 -0300

    first commit

---
+++ ftp_downloader.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ "$1" == "-help" ]
+then
+  printf "\n#\n"
+  printf "\nEl script lista la carperta de nuevos del ftp, descarga"
+  printf " \nlos archivos y los mueve a la carpeta de consumidos"
+else
+  LINK_FTP_EXTERNO='ftp://sftp.despegar.com/upload/'
+  LINK_FTP_ATALAYA='ftp://sftp.despegar.com/upload/'
+
+  printf "\n Iniciando proceso\n"
+     LISTADO_FTP_EXTERNO=$( curl $LINK_FTP_EXTERNO  --user worldpay:gj45Gxn7Df -ll)
+     LISTADO_FTP_ATLAYA=$( curl $LINK_FTP_ATALAYA  --user test:test -ll)
+     echo "$LISTADO_FTP_EXTERNO"
+     echo "$LISTADO_FTP_EXTERNO" >> listado_ftp_externo.dat
+     echo "$LISTADO_FTP_ATLAYA" >> listado_ftp_ATALAYA.dat
+
+
+
+     file="listado_ftp_externo.dat"
+      while IFS= read -r line
+      do
+        if grep -q $line "$LISTADO_FTP_ATLAYA"; then
+          	printf '%s' "$line"
+            printf '\n========================\n'
+            curl $LINK_FTP_EXTERNO$line  --user worldpay:gj45Gxn7Df
+            curl -T   $line $LINK_FTP_ATALAYA --user test:test
+        fi
+
+
+      done <"$file"
+
+fi
